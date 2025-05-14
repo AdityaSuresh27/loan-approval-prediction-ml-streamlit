b4017923810d7aa4fd0e25a7bd22d7cc6e8aea93
AdityaSuresh27 <145009301+AdityaSuresh27@users.noreply.github.com>
2025-05-14 23:48:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import pandas as pd
 import joblib
+#First time using streamlit so site is pretty basic, further enhancements can be done 
 
-# Load model and encoders
 model = joblib.load("loan_model.pkl")
 le_dict = joblib.load("encoders.pkl")
 
 st.title("🏦 Loan Approval Predictor")
 
-# Input fields
+
 Gender = st.selectbox("Gender", ["Male", "Female"])
 Married = st.selectbox("Married", ["Yes", "No"])
 Dependents = st.selectbox("Dependents", [0, 1, 2, 3])
@@ -18,10 +18,10 @@ ApplicantIncome = st.number_input("Applicant Income", min_value=0)
 CoapplicantIncome = st.number_input("Coapplicant Income", min_value=0.0)
 LoanAmount = st.number_input("Loan Amount (in thousands)", min_value=0.0)
 Loan_Amount_Term = st.selectbox("Loan Amount Term", [360.0, 120.0, 180.0, 240.0, 300.0, 84.0, 60.0, 12.0])
-Credit_History = st.selectbox("Credit History (1 = good, 0 = bad)", [1.0, 0.0])
+Credit_History = st.selectbox("Credit History (1 = good, 0 = bad)", [1.0, 0.0]) #Yeah, techincally I am pretty sure credit history has more than only two values but from the csv file I took it like that
 Property_Area = st.selectbox("Property Area", ["Urban", "Rural", "Semiurban"])
 
-# Preprocess input
+
 input_dict = {
     "Gender": le_dict["Gender"].transform([Gender])[0],
     "Married": le_dict["Married"].transform([Married])[0],
